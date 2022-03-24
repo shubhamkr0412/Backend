e686d7396f7fe7402f4d1861a722d65c2504571b
Shubham Kumar <shubham35godda@gmail.com>
2022-03-25 02:16:57 +0530

    created express app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express=require ("express");
+const app=express();
+const port=process.env.PORT || 3000;
+app.get("/",async(req,res)=>{
+    res.send("hello from backend");
+})
+app.listen(port,()=>{
+    console.log(`connection is successfull ${port}`);
+})
