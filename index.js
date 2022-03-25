6c742e2da1397016fc08d15ab48c27462627300c
Shubham Kumar <shubham35godda@gmail.com>
2022-03-25 16:12:59 +0530

    created opening page

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const router=require("./src/routers/bloglist")
 const app=express();
 const port=process.env.PORT || 3000;
 app.use(express.json());
+
 app.use(router);
+app.use(express.static("public"));
 
 app.listen(port,()=>{
     console.log(`connection is successfull ${port}`);
