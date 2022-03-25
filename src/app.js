b3a99a0d1d7a16b9f19bd2addc6fc1f06f0f162c
Shubham Kumar <shubham35godda@gmail.com>
2022-03-25 10:41:37 +0530

    created api

--- src/app.js
+++ src/app.js
@@ -4,9 +4,59 @@ const BlogList=require("../src/models/blog");
 
 const app=express();
 const port=process.env.PORT || 3000;
-app.get("/",async(req,res)=>{
-    res.send("hello from backend");
+app.use(express.json());
+app.post("/blogs",async(req,res)=>{
+   try{
+       const addingNewblog=new BlogList(req.body);
+       console.log(req.body);
+        const insertBlog=await addingNewblog.save();
+        res.status(201).send(insertBlog);
+
+   }catch(e){
+       res.status(400).send(e);
+   }
 })
+app.get("/blogs",async(req,res)=>{
+    try{
+        const getBlog=await BlogList.find({}).sort({"popularity":1});
+         res.send(getBlog);
+ 
+    }catch(e){
+        res.status(400).send(e);
+    }
+ })
+ app.get("/blogs/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const getBlog1=await BlogList.findById({_id});
+         res.send(getBlog1);
+ 
+    }catch(e){
+        res.status(400).send(e);
+    }
+ })
+ app.patch("/blogs/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const getBlog1=await BlogList.findByIdAndUpdate(_id,req.body);
+  
+         res.send(getBlog1);
+ 
+    }catch(e){
+        res.status(500).send(e);
+    }
+ })
+ app.delete("/blogs/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const getBlog1=await BlogList.findByIdAndDelete(_id,req.body);
+  
+         res.send(getBlog1);
+ 
+    }catch(e){
+        res.status(500).send(e);
+    }
+ })
 app.listen(port,()=>{
     console.log(`connection is successfull ${port}`);
 })
