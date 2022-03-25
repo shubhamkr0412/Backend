9f1f9f061ae7ccbf14a5b91afbe9e076cdfb0e3f
Shubham Kumar <shubham35godda@gmail.com>
2022-03-25 11:18:50 +0530

    created route

---
+++ src/routers/bloglist.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const router= new express.Router();
+
+const BlogList=require("../models/blog");
+router.post("/blogs",async(req,res)=>{
+    try{
+        const addingNewblog=new BlogList(req.body);
+        console.log(req.body);
+         const insertBlog=await addingNewblog.save();
+         res.status(201).send(insertBlog);
+ 
+    }catch(e){
+        res.status(400).send(e);
+    }
+ })
+ router.get("/blogs",async(req,res)=>{
+     try{
+         const getBlog=await BlogList.find({}).sort({"popularity":1});
+          res.send(getBlog);
+  
+     }catch(e){
+         res.status(400).send(e);
+     }
+  })
+  router.get("/blogs/:id",async(req,res)=>{
+     try{
+         const _id=req.params.id;
+         const getBlog1=await BlogList.findById({_id});
+          res.send(getBlog1);
+  
+     }catch(e){
+         res.status(400).send(e);
+     }
+  })
+  router.patch("/blogs/:id",async(req,res)=>{
+     try{
+         const _id=req.params.id;
+         const getBlog1=await BlogList.findByIdAndUpdate(_id,req.body);
+   
+          res.send(getBlog1);
+  
+     }catch(e){
+         res.status(500).send(e);
+     }
+  })
+  router.delete("/blogs/:id",async(req,res)=>{
+     try{
+         const _id=req.params.id;
+         const getBlog1=await BlogList.findByIdAndDelete(_id,req.body);
+   
+          res.send(getBlog1);
+  
+     }catch(e){
+         res.status(500).send(e);
+     }
+  })
+  module.exports=router;
